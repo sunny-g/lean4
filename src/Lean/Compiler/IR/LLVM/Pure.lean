ae5545683f2e9ae3f8a79720251e68ee036c5f56
Henrik Böving <hargonix@gmail.com>
2023-04-13 01:13:14 +0200

    WIP: more steps towards syntax

--- src/Lean/Compiler/IR/LLVM/Pure.lean
+++ src/Lean/Compiler/IR/LLVM/Pure.lean
@@ -26,15 +26,16 @@ inductive Ty where
 | float
 | double
 | array (elemty : Ty) (nelem : UInt64)
-deriving Inhabited, BEq
+deriving Inhabited, BEq, Repr
+
+-- ptr gets used as keyword in the LLVM grammar, we need to work around it due to the global syntax table
+declare_syntax_cat ptr_ident
+scoped syntax "ptr" : ptr_ident
 
 scoped syntax llvm_ty " (" (llvm_ty),* ")" : llvm_ty
-scoped syntax "void" : llvm_ty
-scoped syntax "i" noWs num : llvm_ty
-scoped syntax "ptr" : llvm_ty
-scoped syntax "float" : llvm_ty
-scoped syntax "double" : llvm_ty
-scoped syntax "[" num " x " llvm_ty "]" : llvm_ty
+scoped syntax ident : llvm_ty -- this gets limited to valid types
+scoped syntax ptr_ident : llvm_ty
+scoped syntax "[" num " × " llvm_ty "]" : llvm_ty
 
 -- TODO: implement
 -- instance : DecidableEq Ty := fun x y => sorry
@@ -48,27 +49,27 @@ syntax llvm_sym := "@" noWs (num <|> ident)
 syntax llvm_value := llvm_reg -- TODO: extend with LLVM constants on demand
 
 inductive Instruction where
-| alloca (ty : Ty) (name : String)
-| load2 (ty : Ty) (val : Reg) (name : String)
+| alloca (ty : Ty) (name : String) (dest : Reg)
+| load2 (ty : Ty) (val : Reg) (name : String) (dest : Reg)
 | store (val pointer : Reg)
-| gep (ty : Ty) (base : Reg) (ixs : Array Reg)
-| inboundsgep (ty : Ty) (base : Reg) (ixs : Array Reg)
-| sext (val : Reg) (destTy : Ty) -- TODO: add source type argument.
-| zext (val : Reg) (destTy : Ty) -- TODO: add source type argument.
-| sext_or_trunc (val : Reg) (destTy : Ty) -- TODO: remove pseudo-instruction, provide builder.
-| ptrtoint (pointer : Reg) (destTy : Ty)
-| mul (lhs rhs : Reg) (name : String)
-| add (lhs rhs : Reg) (name : String)
-| sub (lhs rhs : Reg) (name : String)
-| not (arg : Reg) (name : String) -- -- TODO: remove pseudo-instruction, provide builder.
-| icmp (pred : LLVM.IntPredicate) (lhs rhs : Reg) (name : String)
-| phi (ty : Ty) (args : Array (BBId × Reg))
+| gep (ty : Ty) (base : Reg) (ixs : Array Reg) (dest : Reg)
+| inboundsgep (ty : Ty) (base : Reg) (ixs : Array Reg) (dest : Reg)
+| sext (val : Reg) (destTy : Ty) (dest : Reg) -- TODO: add source type argument.
+| zext (val : Reg) (destTy : Ty) (dest : Reg) -- TODO: add source type argument.
+| sext_or_trunc (val : Reg) (destTy : Ty) (dest : Reg) -- TODO: remove pseudo-instruction, provide builder.
+| ptrtoint (pointer : Reg) (destTy : Ty) (dest : Reg)
+| mul (lhs rhs : Reg) (name : String) (dest : Reg)
+| add (lhs rhs : Reg) (name : String) (dest : Reg)
+| sub (lhs rhs : Reg) (name : String) (dest : Reg)
+| not (arg : Reg) (name : String) (dest : Reg) -- -- TODO: remove pseudo-instruction, provide builder.
+| icmp (pred : LLVM.IntPredicate) (lhs rhs : Reg) (name : String) (dest : Reg)
+| phi (ty : Ty) (args : Array (BBId × Reg)) (dest : Reg)
 deriving Inhabited, BEq
 
 scoped syntax "alloca" llvm_ty : llvm_instr
-scoped syntax "load" llvm_ty ", " "ptr" llvm_reg : llvm_instr
+scoped syntax "load" llvm_ty ", " ptr_ident llvm_reg : llvm_instr
 scoped syntax "store" llvm_ty llvm_value : llvm_instr
-scoped syntax "getelemenptr" ("inbounds")? llvm_ty ", " "ptr" llvm_reg (llvm_ty llvm_reg)* : llvm_instr
+scoped syntax "getelemenptr" ("inbounds")? llvm_ty ", " ptr_ident llvm_reg (llvm_ty llvm_reg)* : llvm_instr
 scoped syntax "sext" llvm_ty llvm_reg "to" llvm_ty : llvm_instr
 scoped syntax "zext" llvm_ty llvm_reg "to" llvm_ty : llvm_instr
 scoped syntax "sext_or_trunc" llvm_ty llvm_reg "to" llvm_ty : llvm_instr
@@ -84,8 +85,6 @@ scoped syntax "phi" llvm_ty ("[" llvm_value ", " llvm_bb_label "]") : llvm_instr
 
 -- TODO: check why we do not need 'fmul' etc.
 
-abbrev InsertionPoint := BBId
-
 inductive Terminator where
 | default
 | unreachable
@@ -93,6 +92,10 @@ inductive Terminator where
 | condbr (val : Reg) (then_ else_ : BBId)
 | ret (val : Reg)
 | switch (val : Reg) (cases : Array (Reg × BBId)) (default : BBId)
+
+/-- We use Terminator.default to signal that this BB does not have a terminator assigned yet. -/
+instance : Inhabited Terminator where
+  default := Terminator.default
 
 scoped syntax "unreachable" : llvm_terminator
 scoped syntax "br" "label" llvm_bb_label : llvm_terminator
@@ -106,10 +109,13 @@ scoped syntax "switch"  llvm_ty llvm_value ", "
 
 abbrev Arg := String × Ty
 structure BasicBlock where
-  id : BBId
   name : String
-  instrs : Array Instruction
-  terminator : Terminator
+  instrs : Array Instruction := #[]
+  terminator : Terminator := .default
+
+def BasicBlock.pushInstruction (i : Instruction) (bb : BasicBlock)  : BasicBlock :=
+ { bb with instrs := bb.instrs.push i }
+
 
 syntax llvm_assignment := llvm_reg " = " llvm_instr
 scoped syntax ident noWs ":"
@@ -119,13 +125,15 @@ scoped syntax ident noWs ":"
 
 structure FunctionDeclaration where
   name : String
-  args : Array Arg
-  retty : Ty
+  args : Array Arg := #[]
+  retty : Ty := Ty.void
+
 
 scoped syntax "declare" llvm_ty llvm_sym "(" (llvm_ty),* ")" : llvm_function
 
 structure FunctionDefinition extends FunctionDeclaration where
-  body : Array BBId
+  body : Array BBId := #[]
+
 
 scoped syntax "define" llvm_ty llvm_sym "(" (llvm_ty llvm_reg),* ")"
   "{" (llvm_bb)* "}": llvm_function
@@ -147,7 +155,7 @@ inductive Initializer where
 syntax llvm_initializer := ("zeroinitializer" <|> "undef" <|> str)
 
 structure GlobalDefinition extends GlobalDeclaration where
-  initializer : Initializer
+  initializer : Initializer := .zero
 
 scoped syntax llvm_sym " = " ("global" <|> "constant")
   llvm_ty llvm_initializer : llvm_global
@@ -160,6 +168,7 @@ scoped syntax "[llvm_bb|" llvm_bb "]" : term
 scoped syntax "[llvm_function|" llvm_function "]" : term
 scoped syntax "[llvm_global|" llvm_global "]" : term
 
+-- TODO: this is literally BEGGING to be indexed by the type.
 abbrev FunctionId := UInt64
 
 structure BuilderState where
@@ -174,11 +183,223 @@ structure BuilderState where
 
   reg : UInt64
   bbid : UInt64
+  functionId : UInt64
+  globalId : UInt64
 
-  insertionpt : Option InsertionPoint
+  bbInsertionPt? : Option BBId
+  functionInsertionPt? : Option FunctionId
 
-abbrev BuilderT (m : Type → Type) [STWorld β m] (α : Type) := StateRefT BuilderState m α
-abbrev BuilderM := BuilderT IO
+/--
+We wish to allow users to lift into `MonadExceptString`. However, the classic style of
+`BuilderT m α := ...` failed us, since not all monad stacks of interest provide errors
+that throw a `String` directly. For example, `IO` throws an `IO.Error`. In theory,
+Lean's typeclass inference could have inferred a [MonadExceptOf IO String] by combining
+the instance `[MonadExceptOf IO.Error IO]` and `[Coe String IO.Error]` to synthesize a
+`[MonadExceptOf String IO]`. Unfortunately, in practice, this does not happen.
+-- TODO: should we send an issue upstream?
+-- TODO: ask on zulip if this is a bug?
+-/
+class MonadExceptString (m : Type → Type) where
+  throwString : String → m α
+
+instance [Coe String ε] [MonadExcept ε m] : MonadExceptString m where
+  throwString := throw ∘ Coe.coe
+
+class MonadBuilder (m : Type → Type) extends Monad m, MonadState BuilderState m, MonadExceptString m where
+
+instance [Monad m] [MonadState BuilderState m] [MonadExceptString m] : MonadBuilder m where
+
+abbrev BuilderM := StateRefT BuilderState IO
+
+namespace Builder
+
+export MonadExceptString (throwString)
+
+variable {m : Type → Type} [MonadBuilder m]
+
+def regGen : m Reg :=
+  BuilderState.reg <$> getModify (fun s => { s with reg := s.reg + 1})
+
+def modifybb  (bbid : BBId) (f : BasicBlock → BasicBlock) : m Unit :=  do
+  match (← get).bbs.find? bbid with
+  | none => throwString s!"unable to find basic block {bbid}"
+  | .some bb => modify fun s => { s with bbs := s.bbs.insert bbid (f bb)}
+
+def insert (i : Instruction) : m Unit := do
+  match (← get).bbInsertionPt? with
+  | none => -- TODO: improve error message by printing instruction.
+    throwString ↑s!"need insertion point to insert instruction"
+  | some bbid => modifybb (bbid : BBId) (BasicBlock.pushInstruction i)
+
+def insertTerminator (t : Terminator) : m Unit := do
+  match (← get).bbInsertionPt? with
+  | none => -- TODO: improve error message by printing terminator.
+    throwString ↑s!"need insertion point to insert terminator."
+  | some bbid => modifybb (bbid : BBId) (fun bb => { bb with terminator := t})
+
+def buildWithDest (f : Reg → Instruction) : m Reg := do
+  let dest ← regGen
+  insert (f dest)
+  return dest
+
+-- Henrik's proposal: Make `Reg` into `Reg (τ : LLVM.Ty)`
+def Alloca (ty : Ty) (name : String := "") : m Reg :=
+  buildWithDest (Instruction.alloca ty name)
+
+def Load2 (ty : Ty) (val : Reg) (name : String) : m Reg :=
+  buildWithDest (Instruction.load2 ty val name)
+
+def Store (val pointer : Reg) : m Unit := insert (.store val pointer)
+
+def Gep (ty : Ty) (base : Reg) (ixs : Array Reg) : m Reg :=
+  buildWithDest (.gep ty base ixs)
+
+def Inboundsgep (ty : Ty) (base : Reg) (ixs : Array Reg) : m Reg :=
+  buildWithDest (.inboundsgep ty base ixs)
+
+def Sext (val : Reg) (destTy : Ty) : m Reg :=
+  buildWithDest (.sext val destTy)
+
+def Zext (val : Reg) (destTy : Ty) : m Reg :=
+  buildWithDest (.zext val destTy)
+
+def Ptrtoint (pointer : Reg) (destTy : Ty) : m Reg :=
+  buildWithDest (.ptrtoint pointer destTy)
+
+def Mul (lhs rhs : Reg) (name : String) : m Reg :=
+  buildWithDest (.mul lhs rhs name)
+
+def Add (lhs rhs : Reg) (name : String) : m Reg :=
+  buildWithDest (.add lhs rhs name)
+
+def Sub (lhs rhs : Reg) (name : String) : m Reg :=
+  buildWithDest (.sub lhs rhs name)
+
+def Not (arg : Reg) (name : String) : m Reg :=
+  buildWithDest (.not arg name)
+
+def Icmp (pred : LLVM.IntPredicate) (lhs rhs : Reg) (name : String) : m Reg :=
+  buildWithDest (.icmp pred lhs rhs name)
+
+def Phi (ty : Ty) (args : Array (BBId × Reg)) : m Reg :=
+  buildWithDest (.phi ty args)
+
+def Unreachable : m Unit := insertTerminator (.unreachable)
+
+def Br (bbid : BBId) : m Unit :=  insertTerminator (.br bbid)
+
+def Condbr (val : Reg) (then_ else_ : BBId) : m Unit := insertTerminator (.condbr val then_ else_)
+
+def Ret (val : Reg) : m Unit := insertTerminator (.ret val)
+
+def Switch (val : Reg) (cases : Array (Reg × BBId)) (default : BBId): m Unit :=
+  insertTerminator (.switch val cases default)
+
+def appendBB (bb : BasicBlock): m BBId := do
+  let st ← get
+  let bbid := st.bbid
+  if let some i := st.functionInsertionPt? then
+    if let some fn := st.functionDefs.find? i then
+      let fn' := { fn with body := fn.body.push bbid }
+      modify (fun s => {
+        s with
+        bbid := s.bbid + 1,
+        bbs := s.bbs.insert bbid bb,
+        functionDefs := s.functionDefs.insert i fn'
+      })
+      return bbid
+    else
+      throwString s!"The insertion point is {i} but that function definition does not exist"
+  else
+    throwString s!"No function insertion pointer is available"
+
+def setInsertionPoint (bbid : BBId) : m Unit := do
+  modify fun s => { s with bbInsertionPt? := bbid }
+
+def functionIdGen : m FunctionId :=
+  BuilderState.functionId <$> getModify (fun s => { s with functionId := s.functionId + 1})
+
+def buildFunctionDeclaration (decl : FunctionDeclaration) : m FunctionId := do
+  let fid ← functionIdGen
+  modify (fun s => { s with functionDecls := s.functionDecls.insert fid decl })
+  return fid
+
+def buildFunctionDefinition (defn : FunctionDefinition): m FunctionId := do
+  let fid ← functionIdGen
+  modify (fun s => { s with functionDefs := s.functionDefs.insert fid defn })
+  return fid
+
+def setFunctionInsertionPoint (fid : FunctionId) : m Unit :=
+  modify (fun s => { s with functionInsertionPt? := fid })
+
+def globalIdGen : m GlobalId :=
+  BuilderState.globalId <$> getModify (fun s => { s with functionId := s.functionId + 1})
+
+def buildGlobalDeclaration (decl : FunctionDeclaration) : m GlobalId := do
+  let gid ← globalIdGen
+  modify (fun s => { s with functionDecls := s.functionDecls.insert gid decl })
+  return gid
+
+end Builder
+/-
+inductive Instruction where
+| alloca (ty : Ty) (name : String)
+| load2 (ty : Ty) (val : Reg) (name : String)
+| store (val pointer : Reg)
+| gep (ty : Ty) (base : Reg) (ixs : Array Reg)
+| inboundsgep (ty : Ty) (base : Reg) (ixs : Array Reg)
+| sext (val : Reg) (destTy : Ty) -- TODO: add source type argument.
+| zext (val : Reg) (destTy : Ty) -- TODO: add source type argument.
+| sext_or_trunc (val : Reg) (destTy : Ty) -- TODO: remove pseudo-instruction, provide builder.
+| ptrtoint (pointer : Reg) (destTy : Ty)
+| mul (lhs rhs : Reg) (name : String)
+| add (lhs rhs : Reg) (name : String)
+| sub (lhs rhs : Reg) (name : String)
+| not (arg : Reg) (name : String) -- -- TODO: remove pseudo-instruction, provide builder.
+| icmp (pred : LLVM.IntPredicate) (lhs rhs : Reg) (name : String)
+| phi (ty : Ty) (args : Array (BBId × Reg))
+deriving Inhabited, BEq
+
+scoped syntax "alloca" llvm_ty : llvm_instr
+scoped syntax "load" llvm_ty ", " ptr_ident llvm_reg : llvm_instr
+scoped syntax "store" llvm_ty llvm_value : llvm_instr
+scoped syntax "getelemenptr" ("inbounds")? llvm_ty ", " ptr_ident llvm_reg (llvm_ty llvm_reg)* : llvm_instr
+scoped syntax "sext" llvm_ty llvm_reg "to" llvm_ty : llvm_instr
+scoped syntax "zext" llvm_ty llvm_reg "to" llvm_ty : llvm_instr
+scoped syntax "sext_or_trunc" llvm_ty llvm_reg "to" llvm_ty : llvm_instr
+scoped syntax "ptrtoint" llvm_ty llvm_reg "to" llvm_ty : llvm_instr
+scoped syntax "mul" llvm_ty llvm_reg ", " llvm_reg : llvm_instr
+scoped syntax "add" llvm_ty llvm_reg ", " llvm_reg : llvm_instr
+scoped syntax "sub" llvm_ty llvm_reg ", " llvm_reg : llvm_instr
+scoped syntax "not" llvm_ty llvm_reg : llvm_instr
+
+syntax llvm_icmp_op := ("eq" <|> "ne" <|> "ugt" <|> "uge" <|> "ult" <|> "ule" <|> "sgt" <|> "sge" <|> "slt" <|> "sle")
+scoped syntax "icmp" llvm_icmp_op llvm_ty llvm_reg ", " llvm_reg : llvm_instr
+scoped syntax "phi" llvm_ty ("[" llvm_value ", " llvm_bb_label "]") : llvm_instr
+-/
+
+-- scoped syntax "build" llvm_instr : llvm_builder_instr
+-- builder newbb ""
+
+macro_rules
+| `([llvm_ty| void]) => ``(Ty.void)
+| `([llvm_ty| float]) => ``(Ty.float)
+| `([llvm_ty| double]) => ``(Ty.double)
+| `([llvm_ty| ptr]) => ``(Ty.opaquePointer 0)
+| `([llvm_ty| $i:ident]) => do
+  let str := i.getId.toString
+  if str.startsWith "i" then
+    if let some width := str.drop 1 |>.toNat? then
+      return ←``(Ty.int $(Lean.quote width))
+  Macro.throwErrorAt i s!"Unknown LLVM type: {str}"
+| `([llvm_ty| $retty:llvm_ty ( $[$args:llvm_ty],* )]) => do
+  let expanders : Array (TSyntax `term) ← args.mapM fun t => ``([llvm_ty| $t])
+  let args : TSyntax `term ← ``(#[ $[ $expanders ],* ])
+  ``(Ty.function $args [llvm_ty| $retty])
+| `([llvm_ty| [ $n:num × $ty:llvm_ty ]]) =>
+    ``(Ty.array [llvm_ty| $ty] ($n : UInt64))
+| `([llvm_ty| $$($q)]) => `($q)
+| `()
 
 structure InstantiationContext (llvmctx : LLVM.Context) where
   llvmmodule : LLVM.Module llvmctx
